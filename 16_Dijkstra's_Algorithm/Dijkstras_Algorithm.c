d62765d971babbfe4e0ef0ae2756cace92fe9e1c
rajectedguy <rajectedguy@gmail.com>
2026-05-31 18:57:38 +0530

    Dijkstra's Algorithm added

---
+++ 16_Dijkstra's_Algorithm/Dijkstras_Algorithm.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define V 5
+#define INF 9999
+
+int minDistance(int dist[], int visited[]) {
+    int min = INF, min_index;
+
+    for (int v = 0; v < V; v++) {
+        if (!visited[v] && dist[v] < min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+void dijkstra(int graph[V][V], int source) {
+    int dist[V];
+    int visited[V];
+
+    for (int i = 0; i < V; i++) {
+        dist[i] = INF;
+        visited[i] = 0;
+    }
+
+    dist[source] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, visited);
+
+        visited[u] = 1;
+
+        for (int v = 0; v < V; v++) {
+            if (!visited[v] &&
+                graph[u][v] &&
+                dist[u] != INF &&
+                dist[u] + graph[u][v] < dist[v]) {
+
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    printf("Vertex\tDistance from Source\n");
+
+    for (int i = 0; i < V; i++)
+        printf("%d\t%d\n", i, dist[i]);
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 10, 0, 5, 0},
+        {0, 0, 1, 2, 0},
+        {0, 0, 0, 0, 4},
+        {0, 3, 9, 0, 2},
+        {7, 0, 6, 0, 0}
+    };
+
+    dijkstra(graph, 0);
+
+    return 0;
+}
